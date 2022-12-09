aba29ade690c6cca8dd9b2797cbbccdeb39fc9e4
ldamet <1410076+ldamet@users.noreply.github.com>
2022-12-09 12:02:05 +0100

    Version for stackblitz

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
+import './style.css';
 import mapboxgl from 'mapbox-gl';
 import { MapboxLayer } from '@deck.gl/mapbox';
-import { ScenegraphLayer } from "deck.gl";
-import { Deck } from "deck.gl";
+import { ScenegraphLayer } from 'deck.gl';
+import { Deck } from 'deck.gl';
 
+const AIRPLANE =
+  'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/scenegraph-layer/airplane.glb';
+const STADIUM =
+  'https://raw.githubusercontent.com/ldamet/deck-scene-layer/main/stade_de_france_v2.glb';
 // Set your mapbox token here
-mapboxgl.accessToken = process.env.MapboxAccessToken;
-
-
-const AIRPLANE = 'https://raw.githubusercontent.com/visgl/deck.gl-data/master/examples/scenegraph-layer/airplane.glb';
-const STADE_DE_FRANCE = "./stade_de_france_v2.glb";
-
-const MODEL = STADE_DE_FRANCE;
-
+const MAPBOX_TOKEN = '';
+const MODEL = STADIUM;
 
 const INITIAL_VIEW_STATE = {
   latitude: 39.1,
@@ -19,13 +18,15 @@ const INITIAL_VIEW_STATE = {
   zoom: 16,
   maxZoom: 20,
   pitch: 0,
-  bearing: 0
+  bearing: 0,
 };
 
-const MAP_STYLE = 'https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json';
+const MAP_STYLE =
+  'https://basemaps.cartocdn.com/gl/dark-matter-nolabels-gl-style/style.json';
 
 export function renderToDOMMapbox(container) {
   const map = new mapboxgl.Map({
+    accessToken: MAPBOX_TOKEN,
     container,
     style: MAP_STYLE,
     center: [INITIAL_VIEW_STATE.longitude, INITIAL_VIEW_STATE.latitude],
@@ -38,11 +39,16 @@ export function renderToDOMMapbox(container) {
     const poiLayer = new MapboxLayer({
       type: ScenegraphLayer,
       id: 'scenegraph-layer-mapbox',
-      data: [{ name: MODEL, position: [INITIAL_VIEW_STATE.longitude, INITIAL_VIEW_STATE.latitude] }],
+      data: [
+        {
+          name: MODEL,
+          position: [INITIAL_VIEW_STATE.longitude, INITIAL_VIEW_STATE.latitude],
+        },
+      ],
       sizeScale: 1,
       scenegraph: MODEL,
-      getPosition: d => d.position,
-      getOrientation: d => [0, 0, 90]
+      getPosition: (d) => d.position,
+      getOrientation: (d) => [0, 0, 90],
     });
 
     map.addLayer(poiLayer);
@@ -51,7 +57,7 @@ export function renderToDOMMapbox(container) {
   return {
     remove: () => {
       map.remove();
-    }
+    },
   };
 }
 
@@ -63,21 +69,32 @@ export function renderToDOMDeck(container) {
     layers: [
       new ScenegraphLayer({
         id: 'scenegraph-layer-deck',
-        data: [{ name: MODEL, position: [INITIAL_VIEW_STATE.longitude, INITIAL_VIEW_STATE.latitude] }],
+        data: [
+          {
+            name: MODEL,
+            position: [
+              INITIAL_VIEW_STATE.longitude,
+              INITIAL_VIEW_STATE.latitude,
+            ],
+          },
+        ],
         sizeScale: 1,
         scenegraph: MODEL,
-        getPosition: d => d.position,
-        getOrientation: d => [0, 0, 90]
-      })
-    ]
+        getPosition: (d) => d.position,
+        getOrientation: (d) => [0, 0, 90],
+      }),
+    ],
   });
 }
 
 export async function loadAndRenderMapbox(container) {
-  console.log("Render mapbox", container)
+  console.log('Render mapbox', container);
   renderToDOMMapbox(container);
 }
 export async function loadAndRenderDeck(container) {
-  console.log("Render deck", container)
-  renderToDOMDeck(container)
+  console.log('Render deck', container);
+  renderToDOMDeck(container);
 }
+
+loadAndRenderMapbox(document.getElementById('mapboxgl'));
+loadAndRenderDeck(document.getElementById('deckgl'));
